d62335289713ff6d0db2547fb0caf5f9a86639e8
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-03-14 16:38:16 -0400

    fix remaining golint static analysis issues

--- src/config/config.go
+++ src/config/config.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+// GetEnv returns environment variable with fallback/default
 func GetEnv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
@@ -14,6 +15,7 @@ func GetEnv(key, fallback string) string {
 	return value
 }
 
+// CheckConfig ensures environment has been configured with AWS API access credentials
 func CheckConfig() {
 	if os.Getenv("AWS_ACCESS_KEY_ID") == "" {
 		log.Error("*Required* key ID not set for AWS API Access.")
